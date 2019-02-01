1ee60d031c81724585ec38a4c1bff2ea5e7f3cb5
anantkaushik <anant.kaushik2@gmail.com>
2019-02-01 13:47:53 +0530

    DB connectivity added

--- flask_app.py
+++ flask_app.py
@@ -1,10 +1,22 @@
 from flask import Flask
+import sqlite3 as sql
 
 app=Flask(__name__)
 
 @app.route("/")
 def home():
-	return ("<h1>WELCOME to Resume Builder</h1>")
+    fname = "Megha"
+    lname = "Mittal"
+    con=sql.connect("static/info.db")
+    cur=con.cursor()
+    # cur.execute("INSERT INTO user (firstName,lastName)VALUES (?,?)",(fname,lname)) 
+    # con.commit()
+    cur.execute("select * from user")
+    info = cur.fetchall()
+    print(info)
+    cur.close()
+    con.close()
+    return ("<h1>WELCOME to Resume Builder</h1>")
 
 if __name__=="__main__":
 	app.run(debug=True,port=5000)
