6eb930373e6b7c928b8c2d05701e1dc4e5e58938
anantkaushik <anant.kaushik2@gmail.com>
2019-02-02 23:54:24 +0530

    Frontend Connected to DB

--- flask_app.py
+++ flask_app.py
@@ -1,22 +1,23 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 import sqlite3 as sql
 
 app=Flask(__name__)
 
 @app.route("/")
 def home():
-    # fname = "Megha"
-    # lname = "Mittal"
-    # con=sql.connect("static/info.db")
-    # cur=con.cursor()
-    # # cur.execute("INSERT INTO user (firstName,lastName)VALUES (?,?)",(fname,lname)) 
-    # # con.commit()
-    # cur.execute("select * from user")
-    # info = cur.fetchall()
-    # print(info)
-    # cur.close()
-    # con.close()
     return render_template("info.html")
+
+@app.route("/addinfo", methods = ['POST'])
+def addinfo():
+    fname = request.form['fname']
+    lname = request.form['lname']
+    con=sql.connect("static/info.db")
+    cur=con.cursor()
+    cur.execute("INSERT INTO user (firstName,lastName)VALUES (?,?)",(fname,lname)) 
+    con.commit()
+    cur.close()
+    con.close()
+    return ("<h1>Hello "+fname+" "+lname+"</h1>")
 
 if __name__=="__main__":
 	app.run(debug=True,port=5000)
