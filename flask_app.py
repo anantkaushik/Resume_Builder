f1becd651245ef82539105ccc500b9a43d4aafe6
anantkaushik <anant.kaushik2@gmail.com>
2019-02-02 23:40:43 +0530

    Connectivity Established

--- flask_app.py
+++ flask_app.py
@@ -1,22 +1,22 @@
-from flask import Flask
+from flask import Flask, render_template
 import sqlite3 as sql
 
 app=Flask(__name__)
 
 @app.route("/")
 def home():
-    fname = "Megha"
-    lname = "Mittal"
-    con=sql.connect("static/info.db")
-    cur=con.cursor()
-    # cur.execute("INSERT INTO user (firstName,lastName)VALUES (?,?)",(fname,lname)) 
-    # con.commit()
-    cur.execute("select * from user")
-    info = cur.fetchall()
-    print(info)
-    cur.close()
-    con.close()
-    return ("<h1>WELCOME to Resume Builder</h1>")
+    # fname = "Megha"
+    # lname = "Mittal"
+    # con=sql.connect("static/info.db")
+    # cur=con.cursor()
+    # # cur.execute("INSERT INTO user (firstName,lastName)VALUES (?,?)",(fname,lname)) 
+    # # con.commit()
+    # cur.execute("select * from user")
+    # info = cur.fetchall()
+    # print(info)
+    # cur.close()
+    # con.close()
+    return render_template("info.html")
 
 if __name__=="__main__":
 	app.run(debug=True,port=5000)
