dfb932e1570f16838790cd2797de4ba4ead77fcd
anantkaushik <anant.kaushik2@gmail.com>
2019-03-24 16:39:29 +0530

    Ajax Added

--- flask_app.py
+++ flask_app.py
@@ -1,5 +1,5 @@
 from flask import Flask, render_template, request
-import sqlite3 as sql
+import sqlite3 as sql, json
 
 app=Flask(__name__)
 
@@ -19,7 +19,18 @@ def addinfo():
     con.close()
     return ("<h1>Hello "+fname+" "+lname+"</h1>")"""
 
-"""Command1"""
+@app.route('/addbasic', methods = ['GET','POST'])
+def addbasic():
+    name = request.form['uname']
+    email = request.form['uemail']
+    mob = request.form['umobno']
+    objective = request.form['uobjective']
+    address = request.form['uaddress'] if request.form['uaddress'] else "NULL" 
+    linkedin = request.form['ulinkedin'] if request.form['ulinkedin'] else "NULL" 
+    portfolio = request.form['uportfolio'] if request.form['uportfolio'] else "NULL" 
+    print(name,email,mob,objective,address,linkedin,portfolio)
+    #return json.dumps({'status':200, 'edit':edit, 'movid':mov_id})
+    return json.dumps({'status':200})
 
 if __name__=="__main__":
 	app.run(debug=True,port=5000)
