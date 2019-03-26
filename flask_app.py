8954d1415e39a5b5583330c44eb7e42b4d3d8038
anantkaushik <anant.kaushik2@gmail.com>
2019-03-26 15:24:32 +0530

    SignUp Added

--- flask_app.py
+++ flask_app.py
@@ -1,11 +1,47 @@
-from flask import Flask, render_template, request
-import sqlite3 as sql, json
+
+from flask import Flask, render_template, redirect, url_for, request, g, session, send_from_directory
+import sqlite3 as sql,os, json
 
 app=Flask(__name__)
+app.config["CACHE_TYPE"] = "null"
+app.secret_key = os.urandom(24)
+
+@app.route("/signup", methods=["POST"])
+def signup():
+    if request.method == "POST":
+        if request.form['password'] == request.form['repassword']:
+            try:
+                name = request.form['name']
+                emailid = request.form['email']
+                emailid = emailid.lower()
+                password = request.form['password']
+                print(name,emailid,password)
+                con = sql.connect("static/resumebuilder.db")
+                cur = con.cursor()
+                cur.execute("select uid from credentials where email = ?",(emailid,))
+                a = cur.fetchone()
+                if a != None:
+                    return "<h1>User Already Exist</h1>"
+                cur.execute("INSERT INTO credentials (name,password,email)VALUES (?,?,?)",(name,password,emailid))
+                con.commit()
+                cur.close()
+                con.close()
+                return redirect("/")
+            except:
+                cur.close()
+                con.close()
+                return "<h1>Something Went Wrong Try Again!</h1>"
+        else:
+            return "<h1>Password and Confirm password does not matched</h1>"
+    return "<h1>Something Went Wrong Try Again</h1>"
+
+@app.route("/form")
+def form():
+    return render_template("info.html")
 
 @app.route("/")
 def home():
-    return render_template("info.html")
+    return render_template("index.html")
 
 @app.route('/addbasic', methods = ['GET','POST'])
 def addbasic():
